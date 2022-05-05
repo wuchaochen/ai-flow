2bd15119c32a99cb6d5a08143874ae24ce772427
wuchaochen <wuchao_chen@qq.com>
2022-05-05 16:14:51 +0800

    Add proto files.

--- ai_flow/rpc/protobuf/proto/gen_protobuf.sh
+++ ai_flow/rpc/protobuf/proto/gen_protobuf.sh
@@ -23,151 +23,50 @@ current_dir=$(cd "$(dirname "$0")";pwd)
 root_dir=${current_dir}/../..
 echo $root_dir
 
-#generate go file
+PROTO_FILES=( "namespace.proto" "workflow.proto" "schedule.proto" "execution.proto")
+
+# generate go file
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
-  message.proto
+  common.proto
 
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
-  metadata_service.proto
+for i in "${PROTO_FILES[@]}"
+do
+  protoc -I/usr/local/include -I. \
+    -I$GOPATH/src \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
+    --grpc-gateway_out=logtostderr=true:../go \
+    "${i}"
+done
 
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true:../go \
-  metadata_service.proto
 
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
-  model_center_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true:../go \
-  model_center_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
-  scheduling_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true:../go \
-  scheduling_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:../go \
-  metric_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --grpc-gateway_out=logtostderr=true:../go \
-  metric_service.proto
-
-#generate java file
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  message.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  metadata_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --plugin=protoc-gen-grpc-java \
-  --grpc-java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  metadata_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  model_center_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --plugin=protoc-gen-grpc-java \
-  --grpc-java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  model_center_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  metric_service.proto
-
-protoc -I/usr/local/include -I. \
-  -I$GOPATH/src -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --plugin=protoc-gen-grpc-java \
-  --grpc-java_out=${root_dir}/java/client/src/main/java \
-  --proto_path=. \
-  metric_service.proto
-
-#generate python file
+# generate python file
 python3 -m grpc.tools.protoc -I. \
   -I/usr/local/include \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --python_out=.. \
-  message.proto
+  common.proto
 
-python3 -m grpc.tools.protoc -I. \
-  -I/usr/local/include \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=.. \
-  --grpc_python_out=.. \
-  metadata_service.proto
-
-python3 -m grpc.tools.protoc -I. \
-  -I/usr/local/include \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=.. \
-  --grpc_python_out=.. \
-  model_center_service.proto
-
-python3 -m grpc.tools.protoc -I. \
-  -I/usr/local/include \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=.. \
-  --grpc_python_out=.. \
-  scheduling_service.proto
-
-python3 -m grpc.tools.protoc -I. \
-  -I/usr/local/include \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=.. \
-  --grpc_python_out=.. \
-  metric_service.proto
-
-python3 -m grpc.tools.protoc -I. \
-  -I/usr/local/include \
-  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-  --python_out=.. \
-  --grpc_python_out=.. \
-  high_availability.proto
+for i in "${PROTO_FILES[@]}"
+do
+  python3 -m grpc.tools.protoc -I. \
+    -I/usr/local/include \
+    -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    --python_out=.. \
+    --grpc_python_out=.. \
+    "${i}"
+done
 
 cd ..
 
-sed -i -E 's/^import message_pb2 as message__pb2/from \. import message_pb2 as message__pb2/' *pb2*.py
-sed -i -E 's/^import metadata_service_pb2 as metadata__service__pb2/from \. import metadata_service_pb2 as metadata__service__pb2/' *pb2*.py
-sed -i -E 's/^import model_center_service_pb2 as model__center__service__pb2/from \. import model_center_service_pb2 as model__center__service__pb2/' *pb2*.py
-sed -i -E 's/^import scheduling_service_pb2 as scheduling__service__pb2/from \. import scheduling_service_pb2 as scheduling__service__pb2/' *pb2*.py
-sed -i -E 's/^import metric_service_pb2 as metric__service__pb2/from \. import metric_service_pb2 as metric__service__pb2/' *pb2*.py
-sed -i -E 's/^import high_availability_pb2 as high__availability__pb2/from \. import high_availability_pb2 as high__availability__pb2/' *pb2*.py
+IMPORT_WORDS=( "common" "namespace" "workflow" "schedule" "execution" )
+for i in "${IMPORT_WORDS[@]}"
+do
+  w="s/^import ${i}_pb2 as ${i}__pb2/from \. import ${i}_pb2 as ${i}__pb2/"
+  echo $w
+  sed -i -E $w *pb2*.py
+done
 rm -rf *.py-E
